71701cf02782b795f415371738a047b6b240ae13
Simon Green <simon@captaincodeman.com>
2022-12-14 10:10:36 -0700

    update to SvelteKit 1.0.0

---
+++ docs/_app/immutable/chunks/1-d7f761b7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-4d436010.js";export{t as component};
---
+++ docs/_app/immutable/chunks/5-e5932781.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-b57c3041.js";import{default as t}from"../components/pages/ssr/_page.svelte-a99b300c.js";export{t as component,r as universal};
