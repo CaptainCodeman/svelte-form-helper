265f1cc886edae0ba7ca946540086599227b2cfd
Simon Green <simon@captaincodeman.com>
2023-05-23 10:25:55 -0600

    update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,11 @@
 import adapter from '@sveltejs/adapter-static'
-import preprocess from 'svelte-preprocess'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 const prod = process.env.NODE_ENV === 'production'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true,
-		}),
-	],
+	preprocess: [vitePreprocess()],
 
 	kit: {
 		adapter: adapter({
