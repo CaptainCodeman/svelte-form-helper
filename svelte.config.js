f870999197c975bda67ed990594bdfe104ba5a58
Simon Green <simon@captaincodeman.com>
2023-06-22 09:52:34 -0600

    update dependencies
    use github action to deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,12 @@
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
-const prod = process.env.NODE_ENV === 'production'
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [vitePreprocess()],
 
 	kit: {
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs',
-			fallback: null,
-		}),
-		paths: {
-			base: prod ? '/svelte-form-helper' : '',
-		},
+		adapter: adapter(),
 	},
 }
 
